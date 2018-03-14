a87214547ac12d16c41417c624e12e71b7ff869f
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-03-14 17:19:26 +0200

    11926 - Multitasking solved.

--- src/main/java/solved/Uva_11926.java
+++ src/main/java/solved/Uva_11926.java
@@ -1,3 +1,5 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -5,26 +7,101 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.BitSet;
 import java.util.function.Consumer;
 
 /**
- *
+ * 11926 - Multitasking
  */
-public class Main {
+public class Uva_11926 {
 
 
-    private Main() throws IOException, InterruptedException {
+    private Uva_11926() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
 
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(in))) {
 
-            //TODO:
+            while (true) {
+
+                BitSet calendar = new BitSet(1_000_001);
+
+                String line = rd.readLine().trim();
+
+                String[] data = line.split("\\s+");
+
+                int n = Integer.parseInt(data[0]);
+                int m = Integer.parseInt(data[1]);
+
+                if (n == 0 && m == 0) {
+                    break;
+                }
+
+                boolean hasConflictFlag = false;
+
+                for (int i = 0; i < n; ++i) {
+                    String[] oneTimeStr = rd.readLine().trim().split("\\s+");
+
+                    if (hasConflictFlag) {
+                        continue;
+                    }
+
+                    int start = Integer.parseInt(oneTimeStr[0]);
+                    int end = Integer.parseInt(oneTimeStr[1]);
+
+                    boolean curConflict = hasConflict(calendar, start, end);
+
+                    if (curConflict) {
+                        hasConflictFlag = true;
+                    }
+
+                    updateCalendar(calendar, start, end);
+                }
+
+                for (int i = 0; i < m; ++i) {
+                    String[] repeatedStr = rd.readLine().trim().split("\\s+");
+
+                    if (hasConflictFlag) {
+                        continue;
+                    }
+
+                    int start = Integer.parseInt(repeatedStr[0]);
+                    int end = Integer.parseInt(repeatedStr[1]);
+                    int repeatOffset = Integer.parseInt(repeatedStr[2]);
+
+                    while (!hasConflictFlag && start < 1_000_001) {
+
+                        boolean curConflict = hasConflict(calendar, start, end);
+
+                        if (curConflict) {
+                            hasConflictFlag = true;
+                        }
+
+                        updateCalendar(calendar, start, end);
+
+                        start += repeatOffset;
+                        end += repeatOffset;
+                    }
+                }
+
+                out.println(hasConflictFlag ? "CONFLICT" : "NO CONFLICT");
+
+            }
 
             diff();
         }
     }
+
+    private static boolean hasConflict(BitSet calendar, int start, int end) {
+        BitSet conflict = calendar.get(start, end);
+        return conflict.cardinality() > 0 ;
+    }
+
+    private static void updateCalendar(BitSet calendar, int start, int end) {
+        calendar.set(start, end, true);
+    }
+
 
     //------------------------------------------------------------------------------------------------------------------
     // DEBUG part
@@ -71,7 +148,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Uva_11926();
         }
         catch (Exception ex) {
             ex.printStackTrace();
