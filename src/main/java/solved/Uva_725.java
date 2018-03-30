2aa9561da9a3ab25c0ed7a5f8264f7a587bfc276
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-03-30 13:58:46 +0300

    UVA-725 solved.

--- src/main/java/solved/Uva_725.java
+++ src/main/java/solved/Uva_725.java
@@ -1,4 +1,4 @@
-
+package solved;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,22 +10,75 @@ import java.nio.file.Paths;
 import java.util.function.Consumer;
 
 /**
- *
+ * 725 - Division
  */
-public class Main {
+public class Uva_725 {
 
-    private Main() throws IOException, InterruptedException {
+    private Uva_725() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
 
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(in))) {
 
-            //TODO:
+            int cnt = 0;
+
+            while (true) {
+                int n = Integer.parseInt(rd.readLine().trim());
+
+                if (n == 0) {
+                    break;
+                }
+
+                printNumbers(n, out, cnt);
+
+                ++cnt;
+            }
 
             diff();
         }
     }
+
+    private static final int ALL_DIGITS_MASK = (1 << 10) - 1;
+
+    private static void printNumbers(int n, PrintStream out, int cnt) {
+
+        if (cnt != 0) {
+            out.println();
+        }
+
+        boolean hasSolution = false;
+
+        for (int f = 1234; f <= 98765 / n; ++f) {
+            int a = f * n;
+
+            int mask = calculateMask(f) ^ calculateMask(a);
+
+            if (mask == ALL_DIGITS_MASK) {
+                hasSolution = true;
+                out.printf("%05d / %05d = %d%n", a, f, n);
+            }
+        }
+
+        if (!hasSolution) {
+            out.printf("There are no solutions for %d.%n", n);
+        }
+    }
+
+    private static int calculateMask(int baseValue) {
+
+        int value = baseValue;
+
+        // check if starts with '0'
+        int mask = (value < 10_000 ? 1 : 0);
+
+        while (value != 0) {
+            mask |= (1 << value % 10);
+            value /= 10;
+        }
+        return mask;
+    }
+
 
     //------------------------------------------------------------------------------------------------------------------
     // DEBUG part
@@ -55,7 +108,7 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-                .exec(java.lang.String.format("/usr/bin/diff %s %s",
+                .exec(String.format("/usr/bin/diff %s %s",
                         "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
                         "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
 
@@ -72,7 +125,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Uva_725();
         }
         catch (Exception ex) {
             ex.printStackTrace();
