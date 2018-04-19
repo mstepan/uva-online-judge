04e99dd5cece3c2fcb4a806a3d5dfc49c14769dd
Maksym Stepanenko <maksym.stepanenko@opower.com>
2018-04-19 17:28:51 +0300

    UVA-10976 solved.

--- src/main/java/solved/Uva_10976.java
+++ src/main/java/solved/Uva_10976.java
@@ -1,4 +1,4 @@
-
+package solved;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,24 +7,88 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
 /**
  * 10976 - Fractions Again?!
  */
-public class Main {
+public class Uva_10976 {
 
-    private Main() throws IOException, InterruptedException {
+    private Uva_10976() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
 
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(in))) {
 
-            //TODO:
+            while (true) {
+
+                String line = rd.readLine();
+
+                if (line == null) {
+                    break;
+                }
+
+                int k = Integer.parseInt(line.trim());
+
+                List<String> res = countSolutions(k);
+
+                out.println(res.size());
+
+                for (String singleSolution : res) {
+                    out.println(singleSolution);
+                }
+            }
 
             diff();
         }
+    }
+
+    private static List<String> countSolutions(int k) {
+
+        List<String> res = new ArrayList<>();
+
+        for (int y = k + 1; y <= 2 * k; ++y) {
+
+            int[] divAndRem = subtract(k, y);
+
+            if (divAndRem[1] == 0) {
+                int x = divAndRem[0];
+                res.add(String.format("1/%d = 1/%d + 1/%d", k, x, y));
+            }
+        }
+
+        return res;
+    }
+
+    private static int[] subtract(int k, int y) {
+
+        int denom = (k * y) / gcd(k, y);
+
+        int num = denom / k - denom / y;
+
+        int div = denom / num;
+        int rem = denom % num;
+
+        return new int[]{div, rem};
+
+    }
+
+    private static int gcd(int x, int y) {
+
+        int a = x;
+        int b = y;
+
+        while (b != 0) {
+            int rem = a % b;
+            a = b;
+            b = rem;
+        }
+
+        return a;
+
     }
 
     //------------------------------------------------------------------------------------------------------------------
@@ -55,7 +119,7 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-                .exec(java.lang.String.format("/usr/bin/diff %s %s",
+                .exec(String.format("/usr/bin/diff %s %s",
                         "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
                         "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
 
@@ -72,7 +136,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Uva_10976();
         }
         catch (Exception ex) {
             ex.printStackTrace();
