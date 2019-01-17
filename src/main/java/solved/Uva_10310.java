c6ba9614e9ed47da95b371a395d1c1b4d88f8d04
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-01-17 14:25:44 +0200

    UVA-10310 solved.

--- src/main/java/solved/Uva_10310.java
+++ src/main/java/solved/Uva_10310.java
@@ -1,3 +1,5 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -5,32 +7,99 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
+ * https://vjudge.net/problem/UVA-10310
+ *
  * <p>
- * https://vjudge.net/problem
+ * https://uva.onlinejudge.org
  * <p>
  * Profile usage:
  * <p>
  * -agentpath:/Users/mstepan/repo/async-profiler/build/libasyncProfiler.so=start,svg,
  * file=/Users/mstepan/repo/uva-online-judge/src/main/java/uva-profile.svg,event=cpu
  */
-public class Main {
+public class Uva_10310 {
 
-    private Main() throws IOException, InterruptedException {
+    private Uva_10310() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
 
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(in))) {
 
-            //TODO:
+            while(true) {
+
+                String line = rd.readLine();
+
+                if (line == null) {
+                    break;
+                }
+
+                String[] data = line.trim().split("\\s+");
+
+                Location[] holes = new Location[Integer.parseInt(data[0])];
+                Location gopher = new Location(Double.parseDouble(data[1]), Double.parseDouble(data[2]));
+                Location dog = new Location(Double.parseDouble(data[3]), Double.parseDouble(data[4]));
+
+
+                for (int i = 0; i < holes.length; ++i) {
+                    String[] coordinates = rd.readLine().trim().split("\\s+");
+                    holes[i] = new Location(Double.parseDouble(coordinates[0]), Double.parseDouble(coordinates[1]));
+                }
+
+                Optional<Location> escapeHole = findEscapeHole(gopher, dog, holes);
+
+
+                if (escapeHole.isPresent()) {
+                    out.printf("The gopher can escape through the hole at (%.3f,%.3f).%n",
+                               escapeHole.get().x, escapeHole.get().y);
+                }
+                else {
+                    out.println("The gopher cannot escape.");
+                }
+
+                rd.readLine();
+            }
 
             diff();
         }
     }
 
+    private Optional<Location> findEscapeHole(Location gopher, Location dog, Location[] holes) {
+
+        for (Location singleHole : holes) {
+
+            double gohperDistance = singleHole.distance(gopher);
+            double dogDistance = singleHole.distance(dog);
+
+            if (Double.compare(gohperDistance * 2, dogDistance) <= 0) {
+                return Optional.of(singleHole);
+            }
+
+        }
+
+        return Optional.empty();
+    }
+
+    private static final class Location {
+        final double x;
+        final double y;
+
+        Location(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        double distance(Location other) {
+            double dx = x - other.x;
+            double dy = y - other.y;
+
+            return Math.sqrt(dx * dx + dy * dy);
+        }
+    }
 
     /*
 
@@ -77,9 +146,9 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-                .exec(java.lang.String.format("/usr/bin/diff %s %s",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
+                .exec(String.format("/usr/bin/diff %s %s",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
 
         StreamGobbler streamGobbler =
                 new StreamGobbler(process.getInputStream(), System.out::println);
@@ -94,7 +163,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Uva_10310();
         }
         catch (Exception ex) {
             ex.printStackTrace();
