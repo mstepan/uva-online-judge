a6c9170b35d609d0307f0f36c24f9454d73b232c
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-01-18 14:19:13 +0200

    UVA-11450 fully solved.

--- src/main/java/solved/Uva_11450.java
+++ src/main/java/solved/Uva_11450.java
@@ -1,3 +1,5 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -5,11 +7,13 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
 /**
+ * https://vjudge.net/problem/UVA-11450
  * <p>
  * https://vjudge.net/problem
  * <p>
@@ -18,33 +22,85 @@ import java.util.function.Consumer;
  * -agentpath:/Users/mstepan/repo/async-profiler/build/libasyncProfiler.so=start,svg,
  * file=/Users/mstepan/repo/uva-online-judge/src/main/java/uva-profile.svg,event=cpu
  */
-public class Main {
+public class Uva_11450 {
 
-    private Main() throws IOException, InterruptedException {
+    private Uva_11450() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
 
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(in))) {
 
-            //TODO:
+            final int testCasesCnt = Integer.parseInt(rd.readLine().trim());
+
+            for (int testCaseIndex = 0; testCaseIndex < testCasesCnt; ++testCaseIndex) {
+
+                String[] moneyAndGarments = rd.readLine().trim().split("\\s+");
+
+                final int money = Integer.parseInt(moneyAndGarments[0]);
+                final int garmentsCnt = Integer.parseInt(moneyAndGarments[1]);
+
+                List<int[]> garments = new ArrayList<>();
+
+                for (int garmentIndex = 0; garmentIndex < garmentsCnt; ++garmentIndex) {
+                    String[] garmentData = rd.readLine().trim().split("\\s+");
+
+                    int[] curGarments = new int[Integer.parseInt(garmentData[0])];
+
+                    for (int i = 0; i < curGarments.length; ++i) {
+                        curGarments[i] = Integer.parseInt(garmentData[i + 1]);
+                    }
+
+                    garments.add(curGarments);
+                }
+
+                int optSol = findOptinalSolution(garments, money);
+
+                if (optSol >= 0) {
+                    out.println(optSol);
+                }
+                else {
+                    out.println("no solution");
+                }
+            }
 
             diff();
         }
     }
 
-    /*
-    11450 Wedding Shopping
+    private int findOptinalSolution(List<int[]> garments, int money) {
 
-    10911 Forming Quiz Teams
-    11635 Hotel Booking
-    11506 Angry Programmer
-    10243 Fire! Fire!! Fire!!!
-    10717 Mint
-    11512 GATTACA
-    10065 Useless Tile Packers
-     */
+        int[] prevSol = new int[money + 1];
 
+        for (int[] garmentsLine : garments) {
+
+            int[] curSol = new int[money + 1];
+            curSol[0] = Integer.MIN_VALUE;
+
+            for (int i = 1; i < curSol.length; ++i) {
+
+                int maxCur = Integer.MIN_VALUE;
+
+                for (int garCost : garmentsLine) {
+                    if (garCost <= i && prevSol[i - garCost] >= 0) {
+                        maxCur = Math.max(maxCur, garCost + prevSol[i - garCost]);
+                    }
+                }
+
+                curSol[i] = maxCur;
+            }
+
+            prevSol = curSol;
+        }
+
+        int optRes = findMax(prevSol);
+
+        return optRes >= 0 ? optRes : -1;
+    }
+
+    private int findMax(int[] prevSol) {
+        return Arrays.stream(prevSol).max().orElseGet(() -> -1);
+    }
 
     //------------------------------------------------------------------------------------------------------------------
     // DEBUG part
@@ -74,9 +130,9 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-                .exec(java.lang.String.format("/usr/bin/diff %s %s",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
+                .exec(String.format("/usr/bin/diff %s %s",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
 
         StreamGobbler streamGobbler =
                 new StreamGobbler(process.getInputStream(), System.out::println);
@@ -91,7 +147,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Uva_11450();
         }
         catch (Exception ex) {
             ex.printStackTrace();
