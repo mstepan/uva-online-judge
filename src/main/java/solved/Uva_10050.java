88b95288b016a85838979ec69adcc955996fffa6
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-05-08 16:12:07 +0300

    Few uva problems solved.

--- src/main/java/solved/Uva_10050.java
+++ src/main/java/solved/Uva_10050.java
@@ -1,3 +1,5 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -5,28 +7,70 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Consumer;
 
 /**
- * Soundex (https://vjudge.net/problem/UVA-10260)
+ * Hartals (https://vjudge.net/problem/UVA-10050)
  * <p>
  * -agentpath:/Users/mstepan/repo/async-profiler/build/libasyncProfiler.so=start,svg,
  * file=/Users/mstepan/repo/uva-online-judge/src/main/java/uva-profile.svg,event=cpu
  */
-public class Main {
+public class Uva_10050 {
 
-    private Main() throws IOException, InterruptedException {
+
+    private Uva_10050() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
 
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(in))) {
 
-            //TODO:
+            final int testCasesCount = Integer.parseInt(rd.readLine().trim());
 
+            for (int testCase = 0; testCase < testCasesCount; ++testCase) {
+                final int days = Integer.parseInt(rd.readLine().trim());
+
+                final int partiesCount = Integer.parseInt(rd.readLine().trim());
+
+                int[] cycles = new int[partiesCount];
+
+                for (int i = 0; i < cycles.length; ++i) {
+                    cycles[i] = Integer.parseInt(rd.readLine().trim());
+                }
+
+                out.println(calculateLostDays(days, cycles));
+
+            }
 
             diff();
         }
+    }
+
+    private int calculateLostDays(int days, int[] cycles) {
+
+        boolean[] notWorkingDays = new boolean[days + 1];
+
+        for (int i = 6; i < notWorkingDays.length; i += 7) {
+            notWorkingDays[i] = true;
+        }
+
+        for (int i = 7; i < notWorkingDays.length; i += 7) {
+            notWorkingDays[i] = true;
+        }
+
+        Set<Integer> hartsDays = new HashSet<>();
+
+        for (int singleCycle : cycles) {
+            for (int day = singleCycle; day <= days; day += singleCycle) {
+                if (!notWorkingDays[day]) {
+                    hartsDays.add(day);
+                }
+            }
+        }
+
+        return hartsDays.size();
     }
 
     //------------------------------------------------------------------------------------------------------------------
@@ -57,9 +101,9 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-                .exec(java.lang.String.format("/usr/bin/diff %s %s",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
+                .exec(String.format("/usr/bin/diff %s %s",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
 
         StreamGobbler streamGobbler =
                 new StreamGobbler(process.getInputStream(), System.out::println);
@@ -74,7 +118,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Uva_10050();
         }
         catch (Exception ex) {
             ex.printStackTrace();
--- src/main/java/solved/Uva_10260.java
+++ src/main/java/solved/Uva_10260.java
@@ -1,3 +1,5 @@
+package solved;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -5,6 +7,8 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Consumer;
 
 /**
@@ -13,20 +17,82 @@ import java.util.function.Consumer;
  * -agentpath:/Users/mstepan/repo/async-profiler/build/libasyncProfiler.so=start,svg,
  * file=/Users/mstepan/repo/uva-online-judge/src/main/java/uva-profile.svg,event=cpu
  */
-public class Main {
+public class Uva_10260 {
 
-    private Main() throws IOException, InterruptedException {
+
+    private Uva_10260() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
 
         try (BufferedReader rd = new BufferedReader(new InputStreamReader(in))) {
 
-            //TODO:
+            String word = rd.readLine();
 
+            while (word != null) {
+
+                word = word.trim();
+
+                out.println(encodeAsSoundex(word));
+
+                word = rd.readLine();
+            }
 
             diff();
         }
+    }
+
+    private static final Map<Character, Character> SOUNDEX_CODES = new HashMap<>();
+
+    static {
+        SOUNDEX_CODES.put('B', '1');
+        SOUNDEX_CODES.put('F', '1');
+        SOUNDEX_CODES.put('P', '1');
+        SOUNDEX_CODES.put('V', '1');
+
+        SOUNDEX_CODES.put('C', '2');
+        SOUNDEX_CODES.put('G', '2');
+        SOUNDEX_CODES.put('J', '2');
+        SOUNDEX_CODES.put('K', '2');
+        SOUNDEX_CODES.put('Q', '2');
+        SOUNDEX_CODES.put('S', '2');
+        SOUNDEX_CODES.put('X', '2');
+        SOUNDEX_CODES.put('Z', '2');
+
+        SOUNDEX_CODES.put('D', '3');
+        SOUNDEX_CODES.put('T', '3');
+
+        SOUNDEX_CODES.put('L', '4');
+
+        SOUNDEX_CODES.put('M', '5');
+        SOUNDEX_CODES.put('N', '5');
+
+        SOUNDEX_CODES.put('R', '6');
+    }
+
+    private String encodeAsSoundex(String word) {
+
+        StringBuilder buf = new StringBuilder(word.length());
+
+        Character lastEncodedSoundexCh = null;
+
+        for (int i = 0; i < word.length(); ++i) {
+            char ch = word.charAt(i);
+
+            if (SOUNDEX_CODES.containsKey(ch)) {
+                char encodedCh = SOUNDEX_CODES.get(ch);
+
+                if (lastEncodedSoundexCh == null || lastEncodedSoundexCh != encodedCh) {
+                    buf.append(encodedCh);
+                    lastEncodedSoundexCh = encodedCh;
+                }
+            }
+            else {
+                lastEncodedSoundexCh = null;
+            }
+        }
+
+        return buf.toString();
     }
 
     //------------------------------------------------------------------------------------------------------------------
@@ -57,9 +123,9 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-                .exec(java.lang.String.format("/usr/bin/diff %s %s",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
+                .exec(String.format("/usr/bin/diff %s %s",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
 
         StreamGobbler streamGobbler =
                 new StreamGobbler(process.getInputStream(), System.out::println);
@@ -74,7 +140,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Uva_10260();
         }
         catch (Exception ex) {
             ex.printStackTrace();
