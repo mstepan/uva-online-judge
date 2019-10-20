96f4acfcfbf0bc5bd7332d63dc66add4e4223c66
Maksym Stepanenko <maksym.stepanenko@opower.com>
2019-10-20 21:01:22 +0300

    Codeforces 1244G fully solved.

--- src/main/java/codeforces/Codeforces_1244G.java
+++ src/main/java/codeforces/Codeforces_1244G.java
@@ -1,3 +1,5 @@
+package codeforces;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,9 +13,9 @@ import java.util.function.Consumer;
  * G. Парные забеги
  * https://codeforces.com/problemset/problem/1244/G
  */
-public class Main {
+public class Codeforces_1244G {
 
-    private Main() throws IOException, InterruptedException {
+    private Codeforces_1244G() throws IOException, InterruptedException {
 
         InputStream in = createInput();
         PrintStream out = createOutput();
@@ -22,11 +24,87 @@ public class Main {
 
             String line = rd.readLine().trim();
 
-            //TODO:
+            String[] parts = line.split("\\s+");
+
+            long n = Long.parseLong(parts[0]);
+            long expectedSum = Long.parseLong(parts[1]);
+
+            long minPossibleSum = n * (n + 1) / 2;
+
+            if(expectedSum < minPossibleSum ){
+                out.println("-1");
+            }
+            else {
+                int[] arr = createSequentialArray((int) n);
+
+                String arr1Str = toStringWithSpaces(arr);
+
+                long bestLeftSum = findBestArray(arr, (expectedSum - minPossibleSum));
+
+                out.println(expectedSum - bestLeftSum);
+                out.println(arr1Str);
+                out.println(toStringWithSpaces(arr));
+
+                System.out.println();
+            }
 
             diff();
         }
     }
+
+    private long findBestArray(int[] arr, long baseLeftSum) {
+
+        long leftSum = baseLeftSum;
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right && leftSum != 0L) {
+
+            int diff = arr[right] - arr[left];
+
+            if (diff <= leftSum) {
+                swap(arr, left, right);
+                leftSum -= diff;
+                ++left;
+                --right;
+            }
+            else {
+                --right;
+            }
+
+        }
+
+        return leftSum;
+    }
+
+    private void swap(int[] arr, int from, int to) {
+        int temp = arr[from];
+
+        arr[from] = arr[to];
+        arr[to] = temp;
+    }
+
+    private static String toStringWithSpaces(int[] arr) {
+        StringBuilder buf = new StringBuilder(arr.length);
+
+        for (int val : arr) {
+            buf.append(val).append(" ");
+        }
+
+        return buf.toString().trim();
+    }
+
+    private static int[] createSequentialArray(int n) {
+        int[] arr = new int[n];
+
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = i + 1;
+        }
+
+        return arr;
+    }
+
 
     //------------------------------------------------------------------------------------------------------------------
     // DEBUG part
@@ -56,9 +134,9 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-                .exec(java.lang.String.format("/usr/bin/diff %s %s",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
-                                              "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
+                .exec(String.format("/usr/bin/diff %s %s",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out.txt",
+                                    "/Users/mstepan/repo/uva-online-judge/src/main/java/out-actual.txt"));
 
         StreamGobbler streamGobbler =
                 new StreamGobbler(process.getInputStream(), System.out::println);
@@ -73,7 +151,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             DEBUG = (args.length == 1);
-            new Main();
+            new Codeforces_1244G();
         }
         catch (Exception ex) {
             ex.printStackTrace();
