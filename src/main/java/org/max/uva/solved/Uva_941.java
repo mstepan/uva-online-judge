336d42e0a4ba916e4ffd96718cec564504e5238d
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-11-11 13:34:28 +0200

    Cleanup.

--- src/main/java/org/max/uva/solved/Uva_941.java
+++ src/main/java/org/max/uva/solved/Uva_941.java
@@ -1,3 +1,5 @@
+package org.max.uva.solved;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -6,25 +8,114 @@ import java.io.PrintStream;
 import java.lang.invoke.MethodHandles;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
- * <a href="https://vjudge.net/problem/UVA-xxx">UVA-xxx: Title</a>
+ * <a href="https://vjudge.net/problem/UVA-941">UVA-941: Permutations</a>
  */
-public class Main {
+public class Uva_941 {
 
 
     private static void mainLogic() throws IOException, InterruptedException {
         try (PrintStream out = createOutput();
              BufferedReader reader = new BufferedReader(new InputStreamReader(createInput()))) {
 
-            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
-                out.println(line);
+            int samplesCount = Integer.parseInt(reader.readLine().trim());
+
+            for (int i = 0; i < samplesCount; ++i) {
+                String str = reader.readLine().trim();
+                long permutationIndex = Long.parseLong(reader.readLine().trim());
+
+                Optional<String> permutationByIndex = findNthPermutation(str, permutationIndex);
+
+                out.println(permutationByIndex.get());
             }
 
             diff();
         }
+    }
+
+    /**
+     * Use factorial number system to find n-th permutation.
+     * See: https://www.quora.com/How-can-I-mathematically-get-n-th-lexicographic-permutation-of-a-set-without-actually
+     * -writing-them-out
+     * See: https://en.wikipedia.org/wiki/Factorial_number_system
+     */
+    private static Optional<String> findNthPermutation(String str, long permutationIndex) {
+
+        long facValue = fac(str.length());
+
+        if (permutationIndex >= facValue ) {
+            return Optional.empty();
+        }
+
+        final char[] strArr = str.toCharArray();
+        Arrays.sort(strArr);
+
+        final StringBuilder res = new StringBuilder(strArr.length);
+        final CharArrayWithUsed arr = new CharArrayWithUsed(strArr);
+
+        long leftPermIndex = permutationIndex;
+
+        for (int facIndex = str.length() - 1; facIndex > 0; --facIndex) {
+            facValue = facValue / (facIndex+1);
+
+            if (facValue <= leftPermIndex) {
+                int elemIndex = (int) (leftPermIndex / facValue);
+
+                char ch = arr.useAtIndex(elemIndex);
+                res.append(ch);
+
+                leftPermIndex -= (elemIndex * facValue);
+            }
+            else {
+                res.append(arr.useAtIndex(0));
+            }
+        }
+
+        res.append(arr.useAtIndex(0));
+
+        return Optional.of(res.toString());
+    }
+
+    private static class CharArrayWithUsed {
+        final char[] arr;
+        final boolean[] used;
+
+        CharArrayWithUsed(char[] arr) {
+            this.arr = arr;
+            this.used = new boolean[arr.length];
+        }
+
+        public char useAtIndex(int elemIndex) {
+            for (int i = 0, curIndex = 0; i < arr.length; ++i) {
+                if (!used[i]) {
+                    if (curIndex == elemIndex) {
+                        used[i] = true;
+                        return arr[i];
+                    }
+                    ++curIndex;
+                }
+            }
+
+            throw new IllegalArgumentException("All elements are used");
+        }
+    }
+
+    private static long fac(int value) {
+
+        assert value <= 20 : "value is too big to calculate factorial using 'long' as a type";
+
+        long res = 1L;
+
+        for (long i = 2L; i <= value; ++i) {
+            res *= i;
+        }
+
+        return res;
     }
 
 
@@ -87,8 +178,8 @@ public class Main {
         }
 
         Process process = Runtime.getRuntime()
-            .exec(java.lang.String.format("%s %s %s", DIFF_TOOL, getPathFromResourceFolder("out.txt"),
-                                          getPathFromResourceFolder("out-actual.txt")));
+            .exec(String.format("%s %s %s", DIFF_TOOL, getPathFromResourceFolder("out.txt"),
+                                getPathFromResourceFolder("out-actual.txt")));
 
         StreamGobbler streamGobbler =
             new StreamGobbler(process.getInputStream(), System.out::println);
